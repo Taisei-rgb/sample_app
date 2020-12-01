48cd71dc0b84c1a98e1d3e323aff64e27e1aae30
Taisei-rgb <taisei.contact@gmail.com>
2020-12-01 12:03:30 +0900

    統合テストの実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-    
-    else  
+    else
+      flash.now[:danger] = "Invalid email/password combination"
       render 'new'
     end
   end
