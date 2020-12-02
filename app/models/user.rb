c3a30147bf97fb2e9cc1e3b2384e74cb91e76b62
Taisei-rgb <taisei.contact@gmail.com>
2020-12-02 14:38:59 +0900

    ユーザーを忘れる + エラーの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,15 +16,19 @@ class User < ApplicationRecord
   end
 
   def User.new_token
-    SecureRandum.urlsafe_base64
+    SecureRandom.urlsafe_base64
   end
 
-  def remember 
+  def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
